a36e766daa76615243d9ef0fec0641a38e234d5f
tiko02220 <tiko02220@gmail.com>
2021-10-06 18:54:00 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+class Main {
+    static int a, b;
+    static String[][] bord;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter size of Grid");
+        a = in.nextInt();
+        b = in.nextInt();
+        bord = new String[a][b];
+        printBord();
+        for (int i = 0;i<=a*b;i++) {
+            System.out.println("Enter X/O");
+            String user = in.next();
+            System.out.println("Enter Index");
+            if (user.equals("X")) {
+                bord[in.nextInt()][in.nextInt()] = "X";
+                printBord();
+            }else if (user.equals("O")) {
+                bord[in.nextInt()][in.nextInt()] = "O";
+                printBord();
+            }
+        }
+    }
+
+    private static void printBord() {
+        for (int c = 0; c < bord.length; c++) {
+            for (int d = 0; d < bord[c].length; d++) {
+if(bord [c][d]== null) {
+            bord [c][d] = "-";
+                  }
+            System.out.print(" | "+ bord[c][d] );
+               }
+            System.out.println(" | ");
+          }
+    }
+}
